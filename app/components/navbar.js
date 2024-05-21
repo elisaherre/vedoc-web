501d469cd2f307a9139ecac3ce69584e5d1d51a2
Elisa Herrera <elisaherrera903@gmail.com>
2024-05-21 12:19:35 -0600

    navbar desktop done

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -5,46 +5,47 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <header>
-      <div className='flex navbar'>
+    <div className='flex navbar'>
+      <Image
+        src='/images/vedoc_logo.png'
+        alt='Vedoc Logo'
+        width={100}
+        height={70}
+        quality={100}
+      />
+      <nav>
+        <ul className='flex'>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About Us</Link>
+          </li>
+          <li>
+            <Link href="/autopros">Auto Pros</Link>
+          </li>
+          <li>
+            <Link href="/faqs">FAQs</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+      <Link href="#" className='button-lg'>Download App</Link>
+      <Link href="#" className='burguer-menu'>
         <Image
-          src='/images/vedoc_logo.png'
-          alt='Vedoc Logo'
-          width={100}
-          height={70}
-          quality={100}
+          src='/images/menu.png'
+          alt='Menu'
+          width={30}
+          height={30}
         />
-        <nav>
-          <ul className='flex'>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About Us</Link>
-            </li>
-            <li>
-              <Link href="/autopros">Auto Pros</Link>
-            </li>
-            <li>
-              <Link href="/faqs">FAQs</Link>
-            </li>
-            <li>
-              <Link href="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact Us</Link>
-            </li>
-          </ul>
-        </nav>
-        <Link href="#" className='button-lg'>Download App</Link>
-        <Link href="#" className='burguer-menu'>
-          <Image
-            src='/images/menu.png'
-            alt='Menu'
-            width={30}
-            height={30}
-          />
-        </Link>
-      </div>
-    </header>
+      </Link>
+    </div>
+  </header>
+
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,3 +19,4 @@ module.exports = {
     require("flowbite/plugin")
   ],
 };
+
