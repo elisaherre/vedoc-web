a7c4d5ea0b40246f0b32af47444dc6b155984e39
Elisa Herrera <elisaherrera903@gmail.com>
2024-05-28 20:49:49 -0600

    dropddown menu progress

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,11 +1,25 @@
+"use client";
+
 import '../globals.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 export default function Navbar() {
+
+  const pathname = usePathname();
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  }
+
   return (
     <header>
     <div className='flex navbar'>
+
+      {/* Logo */}
       <Image
         src='/images/vedoc_logo.png'
         alt='Vedoc Logo'
@@ -13,30 +27,34 @@ export default function Navbar() {
         height={70}
         quality={100}
       />
+
+      {/* Desktop Version */}
       <nav>
-        <ul className='flex'>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About Us</Link>
-          </li>
-          <li>
-            <Link href="/autopros">Auto Pros</Link>
-          </li>
-          <li>
-            <Link href="/faqs">FAQs</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact Us</Link>
-          </li>
-        </ul>
+      <ul className='flex'>
+        <li>
+          <Link href="/" className={pathname === '/' ? 'active' : ''}>Home</Link>
+        </li>
+        <li>
+          <Link href="/about" className={pathname === '/about' ? 'active' : ''}>About Us</Link>
+        </li>
+        <li>
+          <Link href="/autopros" className={pathname === '/autopros' ? 'active' : ''}>Auto Pros</Link>
+        </li>
+        <li>
+          <Link href="/faqs" className={pathname === '/faqs' ? 'active' : ''}>FAQs</Link>
+        </li>
+        <li>
+          <Link href="/blog" className={pathname === '/blog' ? 'active' : ''}>Blog</Link>
+        </li>
+        <li>
+          <Link href="/contact" className={pathname === '/contact' ? 'active' : ''}>Contact Us</Link>
+        </li>
+      </ul>
       </nav>
       <Link href="#" className='button-lg'>Download App</Link>
-      <Link href="#" className='burguer-menu'>
+
+      {/* Mobile Version */}
+      <Link href="#" className='burguer-menu' onClick={toggleDropdown}>
         <Image
           src='/images/menu.png'
           alt='Menu'
@@ -45,6 +63,32 @@ export default function Navbar() {
         />
       </Link>
     </div>
+
+    {dropdownVisible && (
+      <div className='dropdown-menu'>
+        <ul className=''>
+          <li>
+            <Link href="/" className={pathname === '/' ? 'active' : ''}>Home</Link>
+          </li>
+          <li>
+            <Link href="/about" className={pathname === '/about' ? 'active' : ''}>About Us</Link>
+          </li>
+          <li>
+            <Link href="/autopros" className={pathname === '/autopros' ? 'active' : ''}>Auto Pros</Link>
+          </li>
+          <li>
+            <Link href="/faqs" className={pathname === '/faqs' ? 'active' : ''}>FAQs</Link>
+          </li>
+          <li>
+            <Link href="/blog" className={pathname === '/blog' ? 'active' : ''}>Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact" className={pathname === '/contact' ? 'active' : ''}>Contact Us</Link>
+          </li>
+          <Link href="#" className='button-md'>Download App</Link>
+        </ul>
+      </div>
+    )}
   </header>
 
   )
